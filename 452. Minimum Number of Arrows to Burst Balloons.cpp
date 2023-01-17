b3bf259a6935eb65c37593235d1d90d9f3889978
Priyanka Kumari <80983161+PriyankaKumari-2002@users.noreply.github.com>
2023-01-18 01:27:34 +0530

    Create 452. Minimum Number of Arrows to Burst Balloons.cpp

---
+++ 452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool static comp(vector<int>&a, vector<int>&b)
+    {
+        return a[1] < b[1];
+    }
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.size() == 0)
+            return 0;
+        int arrows = 1;
+        sort(points.begin(), points.end(), comp);
+        int last = points[0][1];
+        for(int i=1; i<points.size(); i++)
+        {
+            if(points[i][0]> last)
+            {
+                arrows ++;
+                last = points[i][1];
+            }
+        }
+        return arrows;
+    }
+};
